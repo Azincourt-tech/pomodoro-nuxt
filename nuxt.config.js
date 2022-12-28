f00226bb408c9cfc27a62e152d3756f78ca5ce44
Willian da Silva <willian.almeida@aliare.co>
2022-12-28 18:04:10 -0300

    Ajustes nos arquivos de configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,25 +3,24 @@ export default {
 	head: {
 		title: 'pomodoro-nuxt',
 		htmlAttrs: {
-			lang: 'en'
+			lang: 'en',
 		},
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{ hid: 'description', name: 'description', content: '' },
-			{ name: 'format-detection', content: 'telephone=no' }
 		],
 		link: [
 			{ rel: 'icon', type: 'image/png', href: '/favicon.png' },
 			{ rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap' }
-		]
+			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap' },
+		],
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
 	css: [
 		'~/assets/css/global.css',
-		'~/assets/css/components.css'
+		'~/assets/css/components.css',
 	],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -36,19 +35,26 @@ export default {
 		// https://go.nuxtjs.dev/typescript
 		'@nuxt/typescript-build',
 		// https://go.nuxtjs.dev/tailwindcss
-		'@nuxtjs/tailwindcss'
+		'@nuxtjs/tailwindcss',
 	],
 
 	// Modules: https://go.nuxtjs.dev/config-modules
 	modules: [
-		['cookie-universal-nuxt', { alias: 'cookiz' }]
+		['cookie-universal-nuxt', { alias: 'cookiz' }],
 	],
 
 	// Build Configuration: https://go.nuxtjs.dev/config-build
 	build: {
+		babel: {
+			plugins: [
+				['@babel/plugin-proposal-class-properties', { loose: true }],
+				['@babel/plugin-proposal-private-methods', { loose: true }],
+				['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+			],
+		},
 	},
 
 	tailwindcss: {
-		viewer: false
-	}
+		viewer: false,
+	},
 };
