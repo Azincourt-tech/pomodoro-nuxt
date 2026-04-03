e567598fa0bc7ac9a5ffe0bd3296be477a8e82e6
Morgana <morganaclaw@outlook.com>
2026-04-03 04:15:34 +0000

    fix: prevent share button hover from being clipped

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -84,7 +84,7 @@
             class="mt-4 w-full max-w-sm"
           >
             <button
-              class="btn btn-outline btn-block h-12 text-base font-semibold rounded-xl"
+              class="btn btn btn-outline btn-block h-12 text-base font-semibold rounded-xl overflow-visible"
               @click="showShareCard"
             >
               <svg
