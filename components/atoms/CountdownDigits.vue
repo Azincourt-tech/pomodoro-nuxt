8d03c8bd93ee8b243416c81fe60efc461ccb43ce
Morgana <morganaclaw@outlook.com>
2026-04-03 02:31:29 +0000

    Revert "Merge remote-tracking branch 'origin/feat/redesign-home-layout' into develop"
    This reverts commit b18993b37b509bf12acee8caeea6a510361286e7, reversing
    changes made to dc01ae915cc5d2c578f1ee1740a036e13af954b9.

--- components/atoms/CountdownDigits.vue
+++ components/atoms/CountdownDigits.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex gap-x-1 md:gap-x-2">
+  <div class="w-1/2 flex gap-x-2">
     <span
       v-for="(digit, index) in splittedDigits"
       :key="index"
-      class="w-[2em] md:w-[2.5em] bg-base-200 rounded-2xl flex items-center justify-center py-3 md:py-4 font-rajdhani font-bold text-base-content shadow-inner"
+      class="w-1/2 bg-base-200 rounded-2xl flex items-center justify-center py-4 text-5xl md:text-7xl lg:text-8xl font-rajdhani font-bold text-base-content shadow-inner"
     >
       {{ digit }}
     </span>
